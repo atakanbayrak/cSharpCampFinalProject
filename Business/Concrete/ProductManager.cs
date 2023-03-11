3ce4fe09f54791d2389b3706011acfcdd6626828
Atakan Bayrak <atakanbayrak461@hotmail.com>
2023-03-11 19:25:52 +0300

    İş katmanı methodları şekillendirildi.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -22,5 +22,15 @@ namespace Business.Concrete
         {
             return _productDal.GetAll();
         }
+
+        public List<Product> GetAllByCategoryId(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice <= max && p.UnitPrice >= min);
+        }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,9 +12,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(50,100))
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName); 
             }
 
             
